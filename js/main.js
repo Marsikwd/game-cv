b8e06b55d1a9a106e95e7b4227a48fa85434dfe8
Marsikwd <108618678+Marsikwd@users.noreply.github.com>
2022-11-13 16:28:55 +0200

    First-commit

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+let linkWeb = document.querySelector('.web');
+
+linkWeb.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Web/');
+})
+
+let linkGame = document.querySelector('.game');
+
+linkGame.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Game-City/');
+})
+
+let linkClovery = document.querySelector('.clovery');
+
+linkClovery.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Clovery/');
+})
+
+let linkRest = document.querySelector('.rest');
+
+linkRest.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Restoran/');
+})
+
+let linkVaw = document.querySelector('.vaw');
+
+linkVaw.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/');
+})
+
+let linkHalloween = document.querySelector('.halloween');
+
+linkHalloween.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Halloween/');
+})
+
+let linkWhale = document.querySelector('.whale');
+
+linkWhale.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/whale/');
+})
+
+let linkVideo = document.querySelector('.bg-video');
+
+linkVideo.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/bg-video/');
+})
+
+let linkOstgame = document.querySelector('.OstGame');
+
+linkOstgame.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/OstGame/');
+})
