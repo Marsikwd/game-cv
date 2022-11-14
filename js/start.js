94cef137f9d417ecfe903120b3b0a0ec0786646b
Marsikwd <108618678+Marsikwd@users.noreply.github.com>
2022-11-14 18:51:21 +0200

    update-url

--- js/start.js
+++ js/start.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function ($) {
 			$('.loader').toggleClass('hide');
 			$('#start1').toggleClass('hide');
 			$(e.currentTarget).attr('disabled', false);
-			window.location.href = "../start.html";
+			window.location.href = "https://marsikwd.github.io/game-resume/start.html";
 		}, 5000)
 	});
 });
