09eaba6e93e6d4bbf1f594f773957b01cffe7ae4
Marsikwd <108618678+Marsikwd@users.noreply.github.com>
2022-11-14 18:47:26 +0200

    add-start

---
+++ js/start.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function ($) {
+	$('#start1').on('click', function (e) {
+
+		$(e.currentTarget).attr('disabled', true);
+		$('#start1').toggleClass('hide');
+		$('.loader').toggleClass('hide');
+
+		setTimeout(function () {
+
+			$('.loader').toggleClass('hide');
+			$('#start1').toggleClass('hide');
+			$(e.currentTarget).attr('disabled', false);
+			window.location.href = "../start.html";
+		}, 5000)
+	});
+});
